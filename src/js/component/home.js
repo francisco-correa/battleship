807ac70eec177190eebc35a64322551477c4900a
francisco-correa <fcorrea.fernandez@gmail.com>
2021-06-12 16:47:14 +0000

    first draft battleShip board game

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,34 @@
 import React from "react";
+import BoardGame from "../component/board";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import battleShip from "../../img/battleShip.jpg";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
 			<p>
-				<img src={rigoImage} />
+				<img
+					style={{
+						width: "300px",
+						height: "200px",
+						borderRadius: "5px"
+					}}
+					src={battleShip}
+				/>
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
+			<h1>Battle Ship Game</h1>
+			<div>
+				<BoardGame></BoardGame>
+			</div>
 			<p>
 				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+				<a href="https://github.com/francisco-correa">
+					Francisco Correa
+				</a>
+				, with <i style={{ color: "red" }} className="fas fa-heart"></i>
 			</p>
 		</div>
 	);
